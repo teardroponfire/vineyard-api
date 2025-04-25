055bd91d74f9bdfd45242cee6f2b032570249770
Kev Bittner <kev.bittner@gmail.com>
2025-04-25 14:47:02 -0500

    Improved error messaging for JWT verification

--- src/middleware/checkAuthentication.js
+++ src/middleware/checkAuthentication.js
@@ -3,13 +3,13 @@ import jwt from 'jsonwebtoken';
 const verifyToken = (req, res, next) => {
 
   const token = req.header('Authorization');
-  if (!token) return res.status(401).json({ error: 'Access denied.' });
+  if (!token) return res.status(401).json({ status: 401, message: 'Unauthorized.' });
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.userId = decoded.userId;
     next();
   } catch (e) {
-    res.status(401).json({ error: 'Invalid token' });
+    res.status(401).json({ status: 401, message: 'Invalid token' });
   }
 
 }
